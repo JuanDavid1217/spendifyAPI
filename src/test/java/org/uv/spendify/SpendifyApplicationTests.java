f66aa1db4c3d308db7efaa3715d5799731a9c1f2
juan <juand_david2001@hotmail.com>
2023-10-28 11:58:53 -0600

    Estructure Base of Proyect

---
+++ src/test/java/org/uv/spendify/SpendifyApplicationTests.java
@@ -0,0 +1,13 @@
+package org.uv.spendify;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class SpendifyApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
