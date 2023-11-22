e03552ea35ea628f600ed79aa0926b2b16b832fb
juan <juand_david2001@hotmail.com>
2023-11-21 22:21:47 -0600

    correcciones

--- src/main/java/org/uv/spendify/CorsConfig.java
+++ src/main/java/org/uv/spendify/CorsConfig.java
@@ -4,7 +4,11 @@
  */
 package org.uv.spendify;
 
+import org.springframework.context.annotation.Bean;
 import org.springframework.context.annotation.Configuration;
+import org.springframework.web.cors.CorsConfiguration;
+import org.springframework.web.cors.UrlBasedCorsConfigurationSource;
+import org.springframework.web.filter.CorsFilter;
 import org.springframework.web.servlet.config.annotation.CorsRegistry;
 import org.springframework.web.servlet.config.annotation.WebMvcConfigurer;
 
@@ -13,12 +17,20 @@ import org.springframework.web.servlet.config.annotation.WebMvcConfigurer;
  * @author juan
  */
 @Configuration
-public class CorsConfig implements WebMvcConfigurer{
-    @Override
-    public void addCorsMappings(CorsRegistry registry) {
-        registry.addMapping("/**")
-                          .allowedOriginPatterns("*")
-                          .allowedMethods("GET", "POST", "PUT", "DELETE")
-                          .allowedHeaders("*");
+public class CorsConfig{
+    @Bean
+    public CorsFilter cors(){
+        UrlBasedCorsConfigurationSource source=new UrlBasedCorsConfigurationSource();
+        CorsConfiguration config=new CorsConfiguration();
+        config.setAllowCredentials(true);
+        config.addAllowedOriginPattern("*");
+        config.addAllowedHeader("*");
+        config.addAllowedMethod("OPTIOS");
+        config.addAllowedMethod("GET");
+        config.addAllowedMethod("POST");
+        config.addAllowedMethod("PUT");
+        config.addAllowedMethod("DELETE");
+        source.registerCorsConfiguration("/**", config);
+        return new CorsFilter(source);
     }
 }
