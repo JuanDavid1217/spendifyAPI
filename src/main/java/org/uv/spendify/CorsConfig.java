e51ac00455eb73aacc232ab670e353fe3a05527f
JuanDavid1217 <124846921+JuanDavid1217@users.noreply.github.com>
2023-11-21 23:33:27 -0600

    Correcciones (#13)
    * Models and repositories created, also folders structure
    
    * Se configura conexion a servidor remoto
    
    * Se configura conexion a servidor remoto
    
    * Update k8s.yml
    
    * Update k8s.yml
    
    * Se crean controllers para usuarios, ingresos, tips, tipo-gastos y tipo-ingresos, tambien se aumenta swagger.
    
    * Se crean los controllers para presupuestos, presupuestos-detalle y notificaciones
    
    * Se terminan controladores faltantes, completando lo planeado.
    
    * Se realizan pruebas unitarias de la clase de utileria Validaciones.
    
    * Se modifican variables de conexión al server BD
    
    * Variables vulnerables se protejen con actions
    
    * Se regresa una version anterior rama DAVID
    
    * "Se añade JWT y Spring Security"
    
    * Update SpendifyApplication.java
    
    * Update SpendifyApplication.java
    
    * Update SpendifyApplication.java
    
    * Update SpendifyApplication.java
    
    * "Se añade el metodo myInfo"
    
    * Update SpendifyApplication.java
    
    * se configura CORS
    
    * correcciones
    
    * correcciones
    
    * correcciones
    
    * correcciones
    
    * correcciones
    
    * correcciones
    
    * correcciones
    
    ---------
    
    Co-authored-by: juan <juand_david2001@hotmail.com>

--- src/main/java/org/uv/spendify/CorsConfig.java
+++ src/main/java/org/uv/spendify/CorsConfig.java
@@ -4,21 +4,43 @@
  */
 package org.uv.spendify;
 
-import org.springframework.context.annotation.Configuration;
-import org.springframework.web.servlet.config.annotation.CorsRegistry;
-import org.springframework.web.servlet.config.annotation.WebMvcConfigurer;
+
+import java.io.IOException;
+import javax.servlet.Filter;
+import javax.servlet.FilterChain;
+import javax.servlet.ServletException;
+import javax.servlet.ServletRequest;
+import javax.servlet.ServletResponse;
+import javax.servlet.http.HttpServletRequest;
+import javax.servlet.http.HttpServletResponse;
+import org.springframework.core.Ordered;
+import org.springframework.core.annotation.Order;
+import org.springframework.stereotype.Component;
+
 
 /**
  *
  * @author juan
  */
-@Configuration
-public class CorsConfig implements WebMvcConfigurer{
+@Component
+@Order(Ordered.HIGHEST_PRECEDENCE)
+public class CorsConfig implements Filter{
+
     @Override
-    public void addCorsMappings(CorsRegistry registry) {
-        registry.addMapping("/**")
-                          .allowedOriginPatterns("*")
-                          .allowedMethods("GET", "POST", "PUT", "DELETE")
-                          .allowedHeaders("*");
+    public void doFilter(ServletRequest sr, ServletResponse sr1, FilterChain fc) throws IOException, ServletException {
+        HttpServletResponse response=(HttpServletResponse) sr1;
+        HttpServletRequest request=(HttpServletRequest) sr;
+        
+        response.setHeader("Access-Control-Allow-Origin", "*");
+        response.setHeader("Access-Control-Allow-Methods", "OPTIONS, GET, POST, PUT, DELETE");
+        response.setHeader("Access-Control-Allow-Headers", "*");
+        response.setHeader("Access-Control-Allow-Credentials", "false");
+        response.setHeader("Access-Control-Max-Age", "1800");
+        
+        if("OPTIONS".equalsIgnoreCase(request.getMethod())){
+            response.setStatus(HttpServletResponse.SC_OK);
+        }else{
+            fc.doFilter(sr, sr1);
+        }
     }
 }
